5f7612cab8cf25fd5a3adb67ace2f8a56dbcc21f
Priyanshii <Priyanshik222@gmail.com>
2023-03-27 21:34:46 +0530

    final commit

--- app/src/main/java/com/priyu/firebase2/MainActivity.java
+++ app/src/main/java/com/priyu/firebase2/MainActivity.java
@@ -1,14 +1,65 @@
 package com.priyu.firebase2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.stream.DoubleStream;
+
 public class MainActivity extends AppCompatActivity {
+    EditText ed1,ed2;
+    Button btn;
+    TextView txt1;
+    FirebaseAuth Auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        Auth = FirebaseAuth.getInstance();
+
+        ed1 = findViewById(R.id.editTextTextEmailAddress);
+        ed2 = findViewById(R.id.editTextTextPassword);
+        btn = findViewById(R.id.login);
+        txt1 = findViewById(R.id.account);
+
+        txt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegisterUser.class);
+                startActivity(intent);
+            }
+        });
+
+        if(Auth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),LogOut.class));
+            finish();
+        }
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final String smail = ed1.getText().toString();
+                final String spass = ed2.getText().toString();
+                Auth.signInWithEmailAndPassword(smail,spass).addOnCompleteListener(task -> {
+                   if (task.isSuccessful()){
+                       Toast.makeText(MainActivity.this,"logged Success ",Toast.LENGTH_SHORT).show();
+                       startActivity(new Intent(getApplicationContext(),LogOut.class));
+                   }else
+                   {
+                       Toast.makeText(MainActivity.this,"something went wrong",Toast.LENGTH_SHORT).show();
+
+                   }
+                });
+            }
+        });
+        }}
--- app/src/main/java/com/priyu/firebase2/RegisterUser.java
+++ app/src/main/java/com/priyu/firebase2/RegisterUser.java
@@ -2,15 +2,60 @@ package com.priyu.firebase2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterUser extends AppCompatActivity {
     EditText email ,password;
-    BU
+    Button btn2;
+    TextView acc1;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        email = findViewById(R.id.editTextTextEmailAddress2);
+        password = findViewById(R.id.editTextTextPassword2);
+        btn2 = findViewById(R.id.register);
+        acc1 = findViewById(R.id.alreadyy);
+
+        auth = FirebaseAuth.getInstance();
+
+        acc1.setOnClickListener(v -> {
+            Intent intent  = new Intent(RegisterUser.this,MainActivity.class);
+            startActivity(intent);
+        });
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final   String uemail = email.getText().toString();
+                final     String upassword = password.getText().toString();
+
+                auth.createUserWithEmailAndPassword(uemail,upassword).addOnCompleteListener(task -> {
+
+                    if (task.isSuccessful()){
+                        Toast.makeText(RegisterUser.this, "user created", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(), LogOut.class));
+                    }else {
+                        Toast.makeText(RegisterUser.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+
+                });
+            }
+        });
+
+
+
     }
 }
